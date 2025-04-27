37e8e550737cf0eaa6d1ef7761bf79b36b3eb0e1
chetankumar-code <chetankumar12538@gmail.com>
2025-04-27 18:06:43 +0530

    Add files via upload

---
+++ task 1/script.js
@@ -0,0 +1,6 @@
+// script.js
+// This is a simple JavaScript file that contains a function to show an alert message when called.
+// This function will be called when the button is clicked
+        function showMessage() {
+            alert("Thanks for visiting my webpage!");
+        }
